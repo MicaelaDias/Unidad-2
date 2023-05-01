25c1a270c01d80964bf16406e76540f99fafc920
MicaelaDias <132285031+MicaelaDias@users.noreply.github.com>
2023-05-01 15:23:35 -0300

    Delete ejercicio5 directory

--- ejercicio5/Principal.py
+++
@@ -1,8 +0,0 @@
-from ManejadorPlan import ListaDePlanes
-from Menu import Menu
-
-if __name__=='__main__':
-    manejador=ListaDePlanes()
-    menu=Menu()
-    manejador.cargarPlanes()
-    menu.lanzarMenu(manejador)
